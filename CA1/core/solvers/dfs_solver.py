2aaefc66688f2030168e4f0cc1ef3155c62c20f3
MSadraA <mo.sadra.abbasi@gmail.com>
2025-10-28 00:21:13 +0330

    completed DFS(using stack) and BFS

--- CA1/core/solvers/dfs_solver.py
+++ CA1/core/solvers/dfs_solver.py
@@ -8,45 +8,34 @@ import time
     At the end, you must return the walked path represented as a list of moves by using use PacmanGame.get_info()
 """
 
-def dfs_recursive_helper(cur_node, cur_path, visited, start_time, timeout):
-        if time.time() - start_time > timeout:
-            return "timeout"
-
-        if cur_node.is_goal():
-            return cur_path
-
-        for child in cur_node.get_next_states():
-            child_hash = child.get_state()
-
-            if child_hash not in visited:
-
-                visited.add(child_hash)
-
-                child_path = cur_path + [child.get_info()]
-
-                result = dfs_recursive_helper(child, child_path, visited, start_time, timeout)
-
-                if result == "timeout":
-                    return "timeout"
-                if result is not None:
-                    return result
-
-        return None
-
 def dfs_solver(game: PacmanGame, timeout=10):
     start_time = time.time()
-    init_path = [game.get_info()]
-    visited = set()
-    result = dfs_recursive_helper( game, init_path, visited, start_time, timeout )
+    initial_history = [game.get_info()]
+    stack = [ (game, initial_history) ]
 
-    if result == "timeout":
-        print("DFS Timeout!")
-        return [game.get_info()]
-    elif result is None:
-        print("DFS No Solution Found (or hit recursion limit).")
-    else:
-        print("DFS Goal Found!")
-        return result
+    visited = set()
+    visited.add(game.get_state())
+
+    while stack:
+        if time.time() - start_time > timeout:
+            print("DFS Timeout!")
+            return [game.get_info()] 
+
+        cur_node , cur_path = stack.pop()
+
+        if cur_node .is_goal():
+            print("DFS Goal Found!")
+            return cur_path 
+
+        for child in cur_node .get_next_states():
+            child_hash = child.get_state()
+            if child_hash not in visited:
+                visited.add(child_hash)
+                child_path = cur_path + [child.get_info()]
+                stack.append( (child, child_path) )
+
+    print("DFS No Solution Found.")
+    return None
     
 
     
